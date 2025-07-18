673dbaaedb4d1c1eeb52738eadbf8b91c34e4ac0
Sadiq Rahman <sadiqrahman3131@gmail.com>
2025-07-18 21:32:35 +0530

    added tut1.cpp and tut3.cpp
    this the code written by me .

---
+++ tut3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//this is the comment in one line 
+/*this
+is 
+multi
+line 
+comment
+*/
+int main() {
+    int sum = 18;  
+    cout<< "Hello World"<< sum;
+    return 0;
+}
