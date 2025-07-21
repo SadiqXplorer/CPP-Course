10c5116a3bcf557b9f0b55d441fc9e0a5c195aa1
Sadiq Rahman <sadiqrahman3131@gmail.com>
2025-07-21 21:53:43 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   int num1 , num2;
+   cout<<"enter the value of num1:";
+   cin>>num1;
+
+   cout<<"Enter the value of num2:3";
+   cin>>num2;
+
+   cout<<"The sum is "<<num1+num2;
+
+    return 0;
+}
---
+++ tut6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//there are two types of header files 
+// 1. system header file like iostream
+// 2. user defined system header file like "this.h"
+
+//#include "this.h" , this will produce error because "this.h" is not available in the directory. 
+
+
+int main(){
+
+    cout<<"this is the hello world program";
+    return 0;
+}
